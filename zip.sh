bb353a7d57e25bbe9268d16d6a3ee606a4c0268d
minh <Lminhphu0304@gmail.com>
2022-05-27 18:17:50 +1200

    Hacky deploy script

--- zip.sh
+++ zip.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+echo "Build for linux architecture"
+
 GOOS=linux GOARCH=amd64 go build -o main main.go
+
+echo "Zip stuff toghether"
 zip main.zip main
+
 chmod 777 main.zip
+
+echo "updating function code"
+
+aws lambda update-function-code \
+    --function-name  spotify \
+    --zip-file fileb://main.zip --publish
+
+echo "all done. Check prod"
